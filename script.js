f794fd4c7488918d7022211595c50c2696b24df7
Charlie <charlielousmeets@gmail.com>
2019-10-29 19:30:28 +0100

    29okt

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var winkelmand = document.getElementsByClassName("mand");
+
+
+var body = document.body;
+var btnMand = document.querySelector('article .mand');
+
+function addProd() {
+    body.classList.add('shop');
+}
+ 
+
+btnMand.addEventListener('click', addProd);
+
 
